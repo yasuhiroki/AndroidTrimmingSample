9385c8f656dbe117f625770c4829a809df9c5249
yasuhiroki <yasuhiroki.duck@gmail.com>
2020-02-23 16:28:26 +0900

    Support decoration icon on CustomTrimmingActivity

--- app/src/main/java/jp/yasuhiroki/duck/androidtrimmingsample/CustomTrimmingActivity.kt
+++ app/src/main/java/jp/yasuhiroki/duck/androidtrimmingsample/CustomTrimmingActivity.kt
@@ -1,19 +1,29 @@
 package jp.yasuhiroki.duck.androidtrimmingsample
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
 import androidx.databinding.DataBindingUtil
 import com.yalantis.ucrop.UCrop
+import com.yalantis.ucrop.callback.BitmapCropCallback
+import jp.ogwork.gesturetransformableview.view.GestureTransformableImageView
 import jp.yasuhiroki.duck.androidtrimmingsample.databinding.ActivityCustomTrimmingBinding
-import java.io.File
+
 
 class CustomTrimmingActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCustomTrimmingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_trimming)
-        val binding = DataBindingUtil.setContentView<ActivityCustomTrimmingBinding>(
+        binding = DataBindingUtil.setContentView<ActivityCustomTrimmingBinding>(
             this,
             R.layout.activity_custom_trimming
         )
@@ -21,5 +31,72 @@ class CustomTrimmingActivity : AppCompatActivity() {
         val src = intent.getParcelableExtra<Uri>("SRC_URI")!!
         val dst = intent.getParcelableExtra<Uri>("DST_URI")!!
         binding.ucropView.cropImageView.setImageUri(src, dst)
+
+        binding.buttonCrop.setOnClickListener {
+            binding.ucropView.cropImageView.cropAndSaveImage(
+                Bitmap.CompressFormat.PNG,
+                90,
+                object : BitmapCropCallback {
+                    override fun onBitmapCropped(
+                        resultUri: Uri,
+                        offsetX: Int,
+                        offsetY: Int,
+                        imageWidth: Int,
+                        imageHeight: Int
+                    ) {
+                        setResultUri(
+                            resultUri,
+                            binding.ucropView.cropImageView.getTargetAspectRatio(),
+                            offsetX,
+                            offsetY,
+                            imageWidth,
+                            imageHeight
+                        )
+                        finish()
+                    }
+
+                    override fun onCropFailure(t: Throwable) {
+                        finish()
+                    }
+                })
+        }
+
+        binding.buttonDeco.setOnClickListener {
+            val imageView = GestureTransformableImageView(this, null)
+            imageView.setImageResource(R.mipmap.ic_launcher)
+            val lp = FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            binding.decoFrame.addView(imageView, lp)
+        }
+    }
+
+
+    private fun setResultUri(
+        uri: Uri?,
+        resultAspectRatio: Float,
+        offsetX: Int,
+        offsetY: Int,
+        imageWidth: Int,
+        imageHeight: Int
+    ) {
+        val decoInfos: ArrayList<DecoInfo> =
+            ArrayList(binding.decoFrame.children.map {
+                val decoImageView = it as GestureTransformableImageView
+                DecoInfo(decoImageView)
+            }.toList())
+
+        setResult(
+            Activity.RESULT_OK, Intent()
+                .putExtra(UCrop.EXTRA_OUTPUT_URI, uri)
+                .putExtra(UCrop.EXTRA_OUTPUT_CROP_ASPECT_RATIO, resultAspectRatio)
+                .putExtra(UCrop.EXTRA_OUTPUT_IMAGE_WIDTH, imageWidth)
+                .putExtra(UCrop.EXTRA_OUTPUT_IMAGE_HEIGHT, imageHeight)
+                .putExtra(UCrop.EXTRA_OUTPUT_OFFSET_X, offsetX)
+                .putExtra(UCrop.EXTRA_OUTPUT_OFFSET_Y, offsetY)
+                .putParcelableArrayListExtra("DATA", decoInfos)
+        )
     }
 }
--- app/src/main/java/jp/yasuhiroki/duck/androidtrimmingsample/MainActivity.kt
+++ app/src/main/java/jp/yasuhiroki/duck/androidtrimmingsample/MainActivity.kt
@@ -3,11 +3,17 @@ package jp.yasuhiroki.duck.androidtrimmingsample
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.util.Log
+import android.widget.FrameLayout
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.yalantis.ucrop.UCrop
 import jp.yasuhiroki.duck.androidtrimmingsample.databinding.ActivityMainBinding
 import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,13 +30,14 @@ class MainActivity : AppCompatActivity() {
 
         binding.button.setOnClickListener {
             binding.imageView.setImageBitmap(null)
+            binding.decoFrame.removeAllViews()
             UCrop.of(src, dst)
                 .withAspectRatio(1f, 1f)
                 .withMaxResultSize(200, 200)
                 .start(this)
         }
         binding.button2.setOnClickListener {
-            binding.imageView.setImageBitmap(null)
+            binding.decoFrame.removeAllViews()
             val intent = Intent(this, CustomTrimmingActivity::class.java)
             intent.putExtra("SRC_URI", src)
             intent.putExtra("DST_URI", dst)
@@ -41,5 +48,72 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         binding.imageView.setImageURI(dst)
+
+        val decoInfos: ArrayList<DecoInfo>? = data?.getParcelableArrayListExtra("DATA")
+        if (decoInfos != null) {
+            for (sc in decoInfos) {
+                val decoImage = ImageView(this, null)
+                decoImage.setImageResource(R.mipmap.ic_launcher)
+                decoImage.rotation = sc.rect
+                decoImage.scaleX = sc.scale
+                decoImage.scaleY = sc.scale
+                decoImage.translationX = sc.marginLeft
+                decoImage.translationY = sc.marginTop
+                binding.decoFrame.addView(
+                    decoImage,
+                    FrameLayout.LayoutParams(sc.layoutWidth, sc.layoutHeight)
+                )
+            }
+        }
+    }
+}
+
+class DecoInfo : Parcelable {
+    var marginLeft: Float
+    var marginTop: Float
+    var scale: Float
+    var rect: Float
+    var layoutWidth: Int
+    var layoutHeight: Int
+
+    constructor(v: ImageView) {
+        marginLeft = v.translationX
+        marginTop = v.translationY
+        scale = v.scaleX
+        rect = v.rotation
+        layoutWidth = v.layoutParams.width
+        layoutHeight = v.layoutParams.height
+    }
+
+    constructor(i: Parcel) {
+        marginLeft = i.readFloat()
+        marginTop = i.readFloat()
+        scale = i.readFloat()
+        rect = i.readFloat()
+        layoutWidth = i.readInt()
+        layoutHeight = i.readInt()
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeFloat(marginLeft)
+        dest.writeFloat(marginTop)
+        dest.writeFloat(scale)
+        dest.writeFloat(rect)
+        dest.writeInt(layoutWidth)
+        dest.writeInt(layoutHeight)
+    }
+
+    companion object CREATOR : Parcelable.Creator<DecoInfo> {
+        override fun createFromParcel(parcel: Parcel): DecoInfo {
+            return DecoInfo(parcel)
+        }
+
+        override fun newArray(size: Int): Array<DecoInfo?> {
+            return arrayOfNulls(size)
+        }
     }
 }
